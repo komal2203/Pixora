345532b07fcd9b28c20e20559bfe7b4d47a91d54
Komal Meena <capkomal2203@gmail.com>
2025-04-03 15:37:29 +0530

    added env file for backend port

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,12 +1,21 @@
 import axios from "axios";
 
-const url = "https://pixora-r2f8.onrender.com";
+// const url = "http://localhost:5000/posts";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const API_URL = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+
+// const url = "https://pixora-r2f8.onrender.com";
+
+const url = `${API_URL}/posts`;
 
 export const fetchPosts = async () => axios.get(url);
 
 export const createPost = (newPost) => axios.post(url, newPost);
 
-export const updatePost = (id, updatedPost) => 
+export const updatePost = (id, updatedPost) =>
   axios.patch(`${url}/${id}`, updatedPost);
 
 export const deletePost = (id) => axios.delete(`${url}/${id}`);
